bd8a628a4fb4050e6f1b7e48749332d8fefb7828
Jong-Han <jonghan@gmail.com>
2021-01-22 01:54:17 +0900

    측정버튼 Toast 메시지

--- app/src/main/java/halla/icsw/acca_kotlin/fragment/MainFragment.kt
+++ app/src/main/java/halla/icsw/acca_kotlin/fragment/MainFragment.kt
@@ -1,10 +1,14 @@
 package halla.icsw.acca_kotlin.fragment
 
+import android.content.Context
+import android.location.LocationManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
@@ -12,15 +16,19 @@ import halla.icsw.acca_kotlin.R
 import halla.icsw.acca_kotlin.databinding.FragmentDrivingRecordBinding
 import halla.icsw.acca_kotlin.databinding.FragmentMainBinding
 
-class MainFragment : Fragment(), View.OnClickListener{
+class MainFragment : Fragment(), View.OnClickListener {
     lateinit var binding: FragmentMainBinding
     lateinit var navController: NavController
 
+    companion object {
+        var mToast: Toast? = null
+    }
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
-        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_main, container,false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false)
         return binding.root
     }
 
@@ -30,13 +38,31 @@ class MainFragment : Fragment(), View.OnClickListener{
         binding.btnDrive.setOnClickListener(this)
         binding.btnMaintenance.setOnClickListener(this)
         binding.btnOil.setOnClickListener(this)
+        binding.btnStart.setOnClickListener(this)
+        binding.btnEnd.setOnClickListener(this)
+
+
+        val lm = activity?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+
     }
 
     override fun onClick(view: View?) {
-        when(view){
-            binding.btnDrive -> navController.navigate(R.id.action_mainFragment_to_drivingRecordFragment)
-            binding.btnOil -> navController.navigate(R.id.action_mainFragment_to_oilRecordFragment)
-            binding.btnMaintenance -> navController.navigate(R.id.action_mainFragment_to_maintenanceFragment)
+        when (view) {
+            binding.btnStart -> {
+                makeToast("시작")
+            }
+            binding.btnEnd -> {
+                makeToast("종료")
+            }
         }
+    }
+
+    fun makeToast(str: String) {
+        if (mToast == null) {
+            mToast = Toast.makeText(context, str, Toast.LENGTH_SHORT)
+        }else
+            mToast?.setText(str)
+        mToast?.show()
     }
 }
