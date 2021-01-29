0bd7bb2ecc4a652d70b8ad11fed2f4956b650008
Jong-Han <jonghan@gmail.com>
2021-01-30 02:31:17 +0900

    SMS판단 로직 추가

--- app/src/main/java/halla/icsw/acca_kotlin/SMSReceiver.kt
+++ app/src/main/java/halla/icsw/acca_kotlin/SMSReceiver.kt
@@ -6,6 +6,10 @@ import android.content.Intent
 import android.telephony.SmsMessage
 import android.util.Log
 import android.widget.Toast
+import halla.icsw.acca_kotlin.DB.OilEntity
+import halla.icsw.acca_kotlin.DB.Repository
+import java.text.SimpleDateFormat
+import java.util.*
 
 class SMSReceiver : BroadcastReceiver() {
 
@@ -13,26 +17,47 @@ class SMSReceiver : BroadcastReceiver() {
         if (intent.action.equals("android.provider.Telephony.SMS_RECEIVED")) {
             Toast.makeText(context, "메시지 수신", Toast.LENGTH_SHORT).show()
             val bundle = intent.extras
-            var message = "" // 출력할 문자열 저장
+            var message = "" // SMS 저장
+            var totalPrice = ""
+            var today = ""
+
 
             if (bundle != null) { // 수신된 내용이 있으면
                 // 실제 메세지는 Object타입의 배열에 PDU 형식으로 저장됨
-                val pdus = bundle["pdus"] as Array<Any>?
+                val pdus = bundle["pdus"] as Array<*>?
                 val msgs = arrayOfNulls<SmsMessage>(
-                    pdus!!.size
+                        pdus!!.size
                 )
                 for (i in msgs.indices) {
                     // PDU 포맷으로 되어 있는 메시지를 복원합니다.
                     if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
-                        msgs[i] = SmsMessage.createFromPdu(pdus!![i] as ByteArray, bundle.getString("format"))
+                        msgs[i] = SmsMessage.createFromPdu(pdus[i] as ByteArray, bundle.getString("format"))
                     } else {
                         @Suppress("DEPRECATION")
-                        msgs[i] = SmsMessage.createFromPdu(pdus!![i] as ByteArray)
+                        msgs[i] = SmsMessage.createFromPdu(pdus[i] as ByteArray)
                     }
-                    message += msgs[i]?.getMessageBody().toString()
+                    message += msgs[i]?.messageBody.toString()
                 }
+
+                var isStation = message.contains("주유소") || message.contains("지에스칼텍스")
+                var isCancel = message.contains("취소") || message.contains("거절")
+                if (isStation && !isCancel) {
+
+                    var timeNow = System.currentTimeMillis()
+                    var date = Date(timeNow)
+                    var sdf = SimpleDateFormat("yyyy-MM-dd")
+                    today = sdf.format(date)
+
+                    var messageArray = message.split("\n", " ")
+                    for (index in messageArray) {
+                        if (index.contains(",")) {
+                            totalPrice = index
+                            break
+                        }
+                    }
+                }
+                Log.d("메시지 처리", today + " " + totalPrice.slice(IntRange(0, totalPrice.indexOf("원"))))
             }
-            Log.d("메시지수신!", message)
         }
     }
 }
